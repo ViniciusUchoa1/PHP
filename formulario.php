3b056cf91ce2736c0c29cb4a3253c7d77cb831d8
ViniciusUchoa1 <42256667+ViniciusUchoa1@users.noreply.github.com>
2018-09-06 20:09:19 -0300

    Create formulario.php

---
+++ formulario.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <form method="GET" action="respostaFormulario.php">
+            Numero de Cadastros: <input type="number" name="numero1"> <br/><br/>
+  
+            <input type="submit" value="Verificar">
+
+        </form>
+    </body>
+</html>
+
