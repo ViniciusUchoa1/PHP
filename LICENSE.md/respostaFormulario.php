e72fc86f0e1d0f981e973f8f677635f088ba34d0
ViniciusUchoa1 <42256667+ViniciusUchoa1@users.noreply.github.com>
2018-09-06 20:04:53 -0300

    Update and rename LICENSE.md to respostaFormulario.php

---
+++ LICENSE.md/respostaFormulario.php
@@ -0,0 +1,16 @@
+<?php
+$num = $_GET['numero1'];
+
+echo "Escreva ".$num." nomes para o cadastro<br/><br/>";
+while ($num > 0 ) {
+    $numero = 0;
+    $numero++;
+    echo "Nome".$numero."<input type="text" none="nomes"><br/><br/>";
+    $num--;
+        
+}
+
+for($quan = 1; $quan > 0; $quan = $quan - 1){
+    echo "Nome<input type="text" none="nomes"><br/><br/>";
+}
+?>
